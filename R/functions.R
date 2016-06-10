90abc45bb2a3aebd46435a6d7baf3b5ddf3d239c
YASH TEKRIWAL <yt2c@virginia.edu>
2016-06-10 17:37:56 +0200

    typos

--- R/functions.R
+++ R/functions.R
@@ -31,16 +31,16 @@ getWords <- function(url) {
 #' @export
 
 getComments <- function(url) {
-  content <- reddit_content(url)
-  comments <- content$comment
-  corpus <- Corpus(VectorSource(url))
+  content <- RedditExtractoR::reddit_content(url)
+  comments <- content$comments
+  corpus <- tm::Corpus(VectorSource(url))
   
-  corpus <- tm_map(corpus, content_transformer(tolower), lazy=T) #lower case conversion
-  corpus <- tm_map(corpus, removePunctuation, lazy=T)
-  corpus <- tm_map(corpus, removeNumbers, lazy=T) #remove numbers
-  corpus <- tm_map(corpus, removeWords, stopwords("english"), lazy=T) #remove stop words
-  corpus <- tm_map(corpus, stripWhitespace, lazy=T) #strip whitespace
-  corpus <- tm_map(corpus, stemDocument, lazy=T)
+  corpus <- tm::tm_map(corpus, content_transformer(tolower), lazy=T) #lower case conversion
+  corpus <- tm::tm_map(corpus, removePunctuation, lazy=T)
+  corpus <- tm::tm_map(corpus, removeNumbers, lazy=T) #remove numbers
+  corpus <- tm::tm_map(corpus, removeWords, stopwords("english"), lazy=T) #remove stop words
+  corpus <- tm::tm_map(corpus, stripWhitespace, lazy=T) #strip whitespace
+  corpus <- tm::tm_map(corpus, stemDocument, lazy=T)
 }
 
 #' Retrieve a wordcloud of the most used positive words in all the comments of the thread passed to posWordCloud
@@ -56,12 +56,12 @@ posWordCloud <- function(words) {
   lex <- read.csv("inquirerbasic.csv", stringsAsFactors=FALSE)
   lex$Entry = gsub("#1", "", lex$Entry)
   lex = lex[!grepl("#", lex$Entry), ]
-  pos.lex = tolower(lex$Entry[lex$Positiv != ""])
+  pos.lex = tm::tolower(lex$Entry[lex$Positiv != ""])
   
   
   pos.comments = words[words %in% pos.lex]
 
-  wordcloud(pos.comments, min.freq = mean(as.numeric(table(pos.comments))), colors = brewer.pal(9, "Blues")[6:9])
+  wordcloud::wordcloud(pos.comments, min.freq = mean(as.numeric(table(pos.comments))), colors = brewer.pal(9, "Blues")[6:9])
   }
 
 #' Retrieve a wordcloud of the most used negative words in all the comments of the thread passed to negWordCloud
@@ -80,11 +80,11 @@ negWordCloud <- function(words) {
   lex$Entry = gsub("#1", "", lex$Entry)
   lex = lex[!grepl("#", lex$Entry), ]
   
-  neg.lex = tolower(lex$Entry[lex$Negativ != ""])
+  neg.lex = tm::tolower(lex$Entry[lex$Negativ != ""])
   
   neg.comments = words[words %in% neg.lex]
   
-  wordcloud(neg.comments, scale = c(4, .5), min.freq = mean(as.numeric(table(neg.comments))), colors = brewer.pal(9, "Reds")[6:9])
+  wordcloud::wordcloud(neg.comments, scale = c(4, .5), min.freq = mean(as.numeric(table(neg.comments))), colors = brewer.pal(9, "Reds")[6:9])
 }
 
 #' Retrieve the difference between the sum total of positive and negative words in the thread
@@ -98,8 +98,8 @@ negWordCloud <- function(words) {
 
 all_feelings <- function(comments) {
   
-  positive <- sapply(corpus, tm_term_score, terms_in_General_Inquirer_categories("Positiv"))
-  negative <- sapply(corpus, tm_term_score, terms_in_General_Inquirer_categories("Negativ"))
+  positive <- sapply(corpus, tm.lexicon.GeneralInquirer::tm_term_score, terms_in_General_Inquirer_categories("Positiv"))
+  negative <- sapply(corpus, tm.lexicon.GeneralInquirer::tm_term_score, terms_in_General_Inquirer_categories("Negativ"))
   
   margin <- positive - negative # negative score means more negative than positive
   print(sum(margin))
